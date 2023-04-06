c88b6df1e10ae8009fee63e3e02fdbdb8531f44c
abolhoseinisina <abolhoseini.sina@gmail.com>
2023-04-06 16:01:46 -0500

    remove tweets list from Home

--- twitter_geocoder/frontend/src/components/Home.js
+++ twitter_geocoder/frontend/src/components/Home.js
@@ -1,51 +1,13 @@
 import React, { Component } from "react";
 import { Col, Container, Row } from "reactstrap";
-import TweetList from "./TweetList";
-import NewTweetModal from "./NewTweetModal";
-import MapView from "./MapView"
-
-import axios from "axios";
-
-import { API_URL } from "../constants";
 
 class Home extends Component {
-  state = {
-    tweets: []
-  };
-
-  componentDidMount() {
-    this.resetState();
-  }
-
-  resetState = () => {
-    this.getTweets();
-  };
-
-  getTweets = () => {
-    axios.get(API_URL).then(res => this.setState({ tweets: res.data }));
-  };
-
   render() {
     return (
       <Container style={{ marginTop: "20px" }}>
         <Row className="justify-content-md-center">
-          <Col md lg={9}>
-            <Row>
-              <Col>
-                <TweetList
-                  tweets={this.state.tweets}
-                  resetState={this.resetState}
-                />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <NewTweetModal create={true} resetState={this.resetState} />
-              </Col>
-            </Row>
-          </Col>
-          <Col md lg={3}>
-            <MapView/>
+          <Col>
+            <h3>This is the homepage of the website!</h3>
           </Col>
         </Row>
       </Container>
