73c4b8de3bbc3f0f0bfa7827f76ef6ca0b431bfc
abolhoseinisina <abolhoseini.sina@gmail.com>
2023-04-06 14:54:41 -0500

    add fields to tweet model

--- twitter_geocoder/tweets/models.py
+++ twitter_geocoder/tweets/models.py
@@ -8,6 +8,8 @@ class Tweet(models.Model):
     mainHighway = models.CharField(max_length=30, default=None, blank=True, null=True)
     geoname = models.CharField(max_length=30, default=None, blank=True, null=True)
     score = models.FloatField(default=1)
+    account = models.CharField(max_length=20, default="@511Alberta", blank=False, null=False)
+    province = models.CharField(max_length=30, default="AB", blank=False, null=False)
 
     def __str__(self):
         return self.name
